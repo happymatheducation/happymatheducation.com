bc911eacf1822a0a84bc87ef3a55a5f003d955b8
Jeffrey Zang <zangj4548@wrdsb.ca>
2022-07-12 14:27:47 -0400

    Build folder again

--- src/App.js
+++ src/App.js
@@ -5,8 +5,8 @@ function App() {
   return (
     <>
     <img src = {logo} alt = 'logo' style = {{display: 'block', marginLeft: 'auto', marginTop: '30px', marginRight: 'auto'}}></img>
-    <div className="container" style = {{backgroundColor: '#898AA6', width: 'max-content'}}>
-      <button className='btn' style = {{display: 'inline', justifySelf: 'center'}}>HOME</button>
+    <div className="container" style = {{display: 'flex', justifyContent: 'space-between', backgroundColor: '', width: 'max-content'}}>
+      <button className='btn' style = {{display: 'inline', justifySelf: 'center', marginRight: '50px'}}>HOME</button>
       <button className='btn' style = {{display: 'inline', justifySelf: 'center'}}>HOME</button>
       <button className='btn' style = {{display: 'inline', justifySelf: 'center'}}>HOME</button>
     </div>
