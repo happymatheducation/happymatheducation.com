d5f8a1ab139361be972de144ca7785d9e6983ea7
Jeffrey Zang <zangj4548@wrdsb.ca>
2022-07-14 20:33:09 -0400

    Add policy

--- src/pages/Tuition.js
+++ src/pages/Tuition.js
@@ -6,7 +6,7 @@ function Tuition() {
     <>    
     <NavBar/>
 
-    <h1 style = {{marginTop: '40px', color: '#898AA6', display: 'flex', justifyContent: 'center', marginBottom: '10px', fontSize: '100px', textAlign: 'center'}}>Tuition</h1>
+    <h1 style = {{marginTop: '20px', color: '#898AA6', display: 'flex', justifyContent: 'center', marginBottom: '10px', fontSize: '120px', textAlign: 'center'}}>Tuition</h1>
     <h1 style = {{color: '#748DA6', fontSize: '30px', textAlign: 'center', marginTop: '-20px'}}>
       {'All classes are held online\u00A0'}
       <i class="bi bi-person-workspace"></i>    
@@ -62,6 +62,70 @@ function Tuition() {
       </h1>
       </div>
     </div>
+
+    <div className = 'container'>
+      <h1 style = {{color: '#51557E', fontSize: '60px', textAlign: 'center'}}>Olympiad Contest Classes</h1>
+      <div style = {{justifyContent: 'space-between'}}>
+      <h1 style = {{color: '#748DA6', fontSize: '30px', textAlign: 'center'}}>
+          {'\u00A0100$/hour/student\u00A0'}
+          <i class="bi bi-people-fill"></i>    
+      </h1>
+      <h1 style = {{display: 'inline', fontSize: '25px', color: ''}}>
+        {'This class will study the following contests: CMO, USAMO, IMO'}
+      </h1>
+      </div>
+    </div>
+
+    <div className = 'container'>
+      <h1 style = {{color: '#51557E', fontSize: '60px', textAlign: 'center'}}>Python/CCC Junior Classes</h1>
+      <div style = {{justifyContent: 'space-between'}}>
+      <h1 style = {{color: '#748DA6', fontSize: '30px', textAlign: 'center'}}>
+          {'\u00A045$/hour/student\u00A0'}
+          <i class="bi bi-code-slash"></i>    
+      </h1>
+      <h1 style = {{display: 'inline', fontSize: '25px', color: ''}}>
+        {'This class will study the basic Python and CCC Junior practice questions'}
+      </h1>
+      </div>
+    </div>
+
+    <div className = 'container'>
+      <h1 style = {{color: '#51557E', fontSize: '60px', textAlign: 'center'}}>CCC Senior Classes</h1>
+      <div style = {{justifyContent: 'space-between'}}>
+      <h1 style = {{color: '#748DA6', fontSize: '30px', textAlign: 'center'}}>
+          {'\u00A0100$/hour/student\u00A0'}
+          <i class="bi bi-code-slash"></i>    
+      </h1>
+      <h1 style = {{display: 'inline', fontSize: '25px', color: ''}}>
+        {'This class will study advanced programming and CCC Senior practice questions'}
+      </h1>
+      </div>
+    </div>
+
+    <div className = 'container'>
+      <h1 style = {{color: '#51557E', fontSize: '60px', textAlign: 'center'}}>One-on-one private tutoring</h1>
+      <div style = {{justifyContent: 'space-between'}}>
+      <h1 style = {{color: '#748DA6', fontSize: '30px', textAlign: 'center'}}>
+          {'\u00A0120$/$150/hour/student\u00A0'}
+          <i class="bi bi-person-fill"></i>    
+      </h1>
+      <h1 style = {{display: 'inline', fontSize: '25px', color: ''}}>
+        1. Scheduled (fixed time every week) : $120 / hour, paid 7 days before the first class of each month. If the student fails to attend the scheduled class, half of the fees for the missed class will be refunded. If the teacher cancels a class, full refund for that class will be issued.
+        <br></br>2. Casual (Booked at least 3 days ahead): $150 / hour, can be paid right before each class.
+      </h1>
+      </div>
+    </div>
+
+    <h1 style = {{marginTop: '20px', color: '#898AA6', display: 'flex', justifyContent: 'center', marginBottom: '10px', fontSize: '120px', textAlign: 'center'}}>Policy</h1>
+    <div className = 'container'style = {{width: '80%'}}><h1 style = {{fontSize: '25px'}}>
+    
+    1. Tuitions are paid before each term starts. If the student quits before the end of the term, no refund will be issued.
+    <br></br><br></br>2. Each student joining the zoom class should use the same name as during registration. If the instructor sees a name that is not on the class list, the student will be removed from the zoom meeting. 
+    <br></br><br></br>3. If the student cannot attend a lesson, the student should inform the instructor at least 72 hours before the lesson starts, in order to get the recording of the lesson. Once the recording is sent to the student, the student should view the recording as soon as possible. The recording will only be available for a short period of time. The recording should never be shared with others without consent from Happy Math Education. Sharing the class recording without consent from Happy Math Education could result in a fine of up to $5000. If the student fails to inform the instructor at least 72 hours before the lesson starts, then the student may not get a recording for the missed lesson.
+    <br></br><br></br>4. Lessons cancelled due to the instructor's absence will be made up. The make-up lessons could be on a different day of the week, or on the same day of the week after the last week of the term. If make-up lessons can not be arranged, a full refund for the cancelled lesson will be issued. 
+    <br></br><br></br>5. The instructor will encourage the students to be on time, take notes during the lesson, and submit homework. However, the instructor takes no responsibility if a student comes late, doesn't take notes, or doesn't submit homework. 
+    
+    </h1></div>
 
     <Copyright/>
     </>
