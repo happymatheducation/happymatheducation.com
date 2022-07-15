c2ed8a7ccdaf4c1be54bb64249ff40ac6365e31b
Jeffrey Zang <zangj4548@wrdsb.ca>
2022-07-15 17:15:57 -0400

    Animation, gradient, cool dropdown thing also math contests page

--- src/App.js
+++ src/App.js
@@ -14,7 +14,7 @@ export const NavBar = () => {
   <>
   <img src = {logo} alt = 'logo' style = {{display: 'block', marginLeft: 'auto', marginTop: '5px', marginRight: 'auto'}}></img>
   <div className="topnav" style = {{zIndex: '6969', position: 'sticky', top: -1, backgroundColor: '#51557E', marginTop: '5px', padding: '5px 10px', marginBottom: '5px', borderWidth: '3px', borderColor: '#51557E', borderStyle: 'solid', borderRadius: '0px'}}>
-    
+  
     <button className='btnnavbar' style = {{padding: '', justifySelf: 'center', marginRight: '30px'}} onClick = {
       () => {
         root.render(
@@ -84,6 +84,8 @@ function App() {
         </h1>
       </div>
     </div>
+
+    {/* <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 1440 320"><path fill="#0099ff" fill-opacity="1" d="M0,288L48,272C96,256,192,224,288,197.3C384,171,480,149,576,165.3C672,181,768,235,864,250.7C960,267,1056,245,1152,250.7C1248,256,1344,288,1392,304L1440,320L1440,320L1392,320C1344,320,1248,320,1152,320C1056,320,960,320,864,320C768,320,672,320,576,320C480,320,384,320,288,320C192,320,96,320,48,320L0,320Z"></path></svg> */}
 
     <div className = 'container' style = {{}}>
       <div style = {{display: 'flex', justifyContent: 'space-between', marginTop: '-10px'}}>
--- src/pages/Contests.js
+++ src/pages/Contests.js
@@ -1,6 +1,9 @@
 import {NavBar, Copyright} from '../App';
+import {useState} from 'react';
 
 function Contests() {
+
+  const setButtonText = useState('Click');
 
   return (
     <>    
@@ -8,9 +11,17 @@ function Contests() {
 
     <h1 style = {{marginTop: '20px', color: '#898AA6', display: 'flex', justifyContent: 'center', marginBottom: '10px', fontSize: '120px', textAlign: 'center'}}>Math Contests</h1>
     <h1 style = {{color: '#748DA6', fontSize: '30px', textAlign: 'center', marginTop: '-20px'}}>
-      {'Select your grade: \u00A0'}
+      {'Select your\u00A0'}
+      <div class="dropdown">
+        <button class="dropbtn">grade</button>
+        <div class="dropdown-content">
+          <a onClick = {
+            () => {console.log('fart')}
+          }>eight</a>
+        </div>
+      </div>
+      {' :'}
     </h1> 
-
     <Copyright/>
     </>
   );
