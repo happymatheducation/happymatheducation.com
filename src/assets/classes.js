0837c9124edf2ceb360c4b9f3a9c5bab7dc73b91
Jeffrey Zang <zangj4548@wrdsb.ca>
2022-08-17 14:49:36 -0400

    Classes Page

--- src/assets/classes.js
+++ src/assets/classes.js
@@ -1,45 +1,53 @@
 let Classes = {
-    teacher: {
-        name: "Teacher",
+    g11: {
+        name: "Grade 11 and 12 Contest Classes",
         description: 
-`Part time opportunity. 
-Starting from $50/hour. 
-Deliver classes. Training will be provided.
+`$45/hour/student
+This class will study the following math contests: COMC/AMC12/CSMC/Euclid/AIME.
 `
     },
-    softwareEngineer: {
-        name: "Software Engineer",
+    g9: {
+        name: 'Grade 9 and 10 Contest Classes',
         description: 
-`1 opening.
-Part time opportunity.
-Website Development. Training will be provided.
+`$40/hour/student
+This class will study the following math contests: CIMC/AMC10/Cayley/Pascal/Galois/Fryer.
 `
     },
-    secretary: {
-        name: "Secretary",
-        description:
-`1 opening.
-Part time opportunity.
-To work closely with the director. Tasks include marketing, coordinating, documenting, answering parents' questions, sending notifications, maintaining social media pages and book stores, write various documents, helping run events, organizing math contests, etc. Training will be provided.
-`
-    },
-    assistant: {
-        name: "Teaching Assistance",
-        description:
-`1 opening.
-Temporary (Co-op) opportunity.
-To prepare and to mark homework, quizzes, and mock contests. Need to know how to use Latex. Training will be provided. 
-`
-    },
-    designer: {
-        name: "Problem Designer",
+    g8: {
+        name: 'Grade 7 and 8 Contest Classes',
         description: 
-`1 opening.
-Volunteer opportunity.
-To prepare "The ultimate practice" sets of problems. Need to know how to use Latex. Training will be provided. 
+`$35/hour/student
+This class will study the following math contests: CMGC/AMC8/Gauss8.
 `
-    }
-
+    },
+    adv: {
+        name: 'Advanced Contest Classes',
+        description: 
+`$100/hour/student
+This class will study the following math contests, at an advanced level: CMO/USAMO/IMO.
+`
+    },
+    cccj: {
+        name: 'Python/CCC Junior',
+        description: 
+`$45/hour/student
+This class will study fundamentals of Python and CCC Junior problems. Taught by Dr. Zhou.
+`
+    },
+    cccs: {
+        name: 'CCC Senior',
+        description: 
+`$100/hour/student
+This class will study CCC Senior problems.
+`
+    },
+    one: {
+        name: 'One on One private classes',
+        description: 
+`1. Scheduled (fixed time every week) : $120 / hour, paid 7 days before the first class of each month. If the student fails to attend the scheduled class, half of the fees for the missed class will be refunded. If the teacher cancels a class, full refund for that class will be issued. 
+2. Casual (Booked at least 3 days ahead): $150 / hour, can be paid right before each class.
+`
+    },
 
 }
 
