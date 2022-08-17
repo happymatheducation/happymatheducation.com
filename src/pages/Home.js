025a5d1b8dbf0fd8d54809d1e15292e86578687f
Jeffrey Zang <zangj4548@wrdsb.ca>
2022-08-17 15:48:26 -0400

    add testimonials

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,4 +1,5 @@
 import React from 'react'
+import { useNavigate } from 'react-router-dom';
 import AOS from 'aos';
 import 'aos/dist/aos.css'; // You can also use <link> for styles
 
@@ -6,14 +7,14 @@ import homeimg from '../assets/home.png'
 import teaching from '../assets/teaching.svg'
 import learning from '../assets/learning.svg'
 import math from '../assets/math.svg'
-
-import { useNavigate } from 'react-router-dom';
+import math2 from '../assets/math2.svg'
+import appreciation from '../assets/appreciation.svg'
 
 const Home = () => {
 
   let navigate = useNavigate();
   AOS.init({
-    duration:800
+    duration: 800
   });
 
   return (
@@ -71,6 +72,35 @@ const Home = () => {
       </button>
       </div>
       <img src = {learning} alt = 'teaching' ></img>
+    </div>
+    
+    <div data-aos = 'fade-right' style = {{display: 'block'}}>
+      <h1 style = {{textAlign: 'center', marginTop: '20%'}}>Student Testimonials</h1>
+      <div style = {{marginTop: '0%'}}>
+        
+        <div className = 'textblock'>
+          <h1 style = {{fontSize: 'max(3vw, 20px)'}}>Charles Zhang, Grade 12</h1>
+          <p>Before I started at Happy Math, I was much less interested in math as a whole. After starting, not only did I improve in writing contests, but I also became more passionate for learning math.</p>
+        </div>
+
+        <img src = {appreciation} alt = 'yes'></img>
+
+      </div>
+
+      <div style = {{marginTop: '0%'}}>
+        <div className = 'textblock'>
+          <h1 style = {{fontSize: 'max(3vw, 20px)', textAlign: 'center'}}>Jeffrey Zang, Grade 10</h1>
+          <p>Not only did Happy Math help me feel more confident in my math skills, it allowed me to understand math in a different way than before. I started to approach problems with a different mindset. Taking classes at Happy Math allowed me to score in the Top 25% of participants in the Pascal Math Contest, 2022.</p>
+        </div>
+      </div>
+
+      <div style = {{marginTop: '0%'}}>
+        <img src = {math2} alt = 'math2'></img>
+        <div className = 'textblock'>
+          <h1 style = {{fontSize: 'max(3vw, 20px)', textAlign: 'center'}}>Jay Ren, Grade 10</h1>
+          <p> Happy Math is a great resource for learning contest math! It's really hard to prepare for contests by yourself, so I'm glad that I decided to take lessons here. It's a really efficient way to improve my skills.</p>
+        </div>
+      </div>
     </div>
 
     <div data-aos = 'fade-up'>
