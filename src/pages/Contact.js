fd9f0ffcf16a2e682a6c30fb1a8c96d9511f3c31
Jeffrey Zang <zangj4548@wrdsb.ca>
2022-08-18 13:28:12 -0400

    fix it again

--- src/pages/Contact.js
+++ src/pages/Contact.js
@@ -1,15 +1,22 @@
 import {React} from 'react'
+import { useNavigate } from 'react-router-dom'
 
 import teacher from '../assets/teacher.svg' 
 
 const Contact = () => {
+  let navigate = useNavigate();
+
   return (
     <div className = 'home'>
 
       <div style = {{textAlign: 'center', display: 'block', marginTop: '0px'}}>
         <h1>Apply</h1>
         <p>
-          To apply to any of our <a href = '/classes'>courses</a>, please contact Dr. Xu using one of the following methods:
+          To apply to any of our <a onClick = {
+            () => {
+              navigate('/classes')
+            }
+          }>courses</a>, please contact Dr. Xu using one of the following methods:
           <br></br>
           <br></br>
           <p style = {{fontWeight: 'bold', fontSize: 'max(2.5vw, 25px)'}}>Phone: 519-729-5631</p>
