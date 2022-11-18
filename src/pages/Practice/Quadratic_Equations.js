4326b36dff6685f56cd04f9162d1d31738dd94a3
xudiwaterloo <118504305+xudiwaterloo@users.noreply.github.com>
2022-11-18 02:41:05 -0500

    Update and rename Quadratic_Equations.html to Quadratic_Equations.js

--- src/pages/Practice/Quadratic_Equations.js
+++ src/pages/Practice/Quadratic_Equations.js
@@ -1,35 +1,7 @@
-<html lang="en">
-  <head>
-    <meta charset="utf-8" />
-    <meta name="viewport" content="width=device-width, initial-scale=1" />
-    <meta name="theme-color" content="#000000" />
-    <meta 
-      name="description"
-      content="Happy Math Education. Our mission: to help kids enjoy maths"
-    />
-    <title>Happy Math Education</title>
-    <link rel="shortcut icon" href="./favicon.ico" />
-    
-    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/katex@0.16.3/dist/katex.min.css" integrity="sha384-Juol1FqnotbkyZUT5Z7gUPjQ9gzlwCENvUZTpQBAPxtusdwFLRy382PSDx5UUJ4/" crossorigin="anonymous">
-    <script defer src="https://cdn.jsdelivr.net/npm/katex@0.16.3/dist/katex.min.js" integrity="sha384-97gW6UIJxnlKemYavrqDHSX3SiygeOwIZhwyOKRfSaf0JWKRVj9hLASHgFTzT+0O" crossorigin="anonymous"></script>
-    <script defer src="https://cdn.jsdelivr.net/npm/katex@0.16.3/dist/contrib/auto-render.min.js" integrity="sha384-+VBxd3r6XgURycqtZ117nYw44OOcIax56Z4dCRWbxyPt0Koah1uHoK0o4+/RRE05" crossorigin="anonymous"></script>
-    <script>document.addEventListener("DOMContentLoaded", function () {
-            renderMathInElement(document.body, {
-                // customised options
-                // • auto-render specific keys, e.g.:
-                delimiters: [
-                    { left: '$$', right: '$$', display: true },
-                    { left: '$', right: '$', display: false },
-                    { left: '\\(', right: '\\)', display: false },
-                    { left: '\\[', right: '\\]', display: true }
-                ],
-                // • rendering keys, e.g.:
-                throwOnError: false
-            });
-        });</script>
-    
-  </head>
-  <body>
+function Practice() {
+
+  return (
+    <>
     <div style= "width: 70%; background-color: #EEFFEE; padding: 20px; margin: auto; border: 3px solid #000000; ">
         <style>
             table, td, th {
@@ -158,5 +130,9 @@
                 }</script>
         </div>
     </div>
-  </body>
-  </html>
+              
+    </>
+  );
+}
+
+export default Practice;
